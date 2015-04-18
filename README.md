a18b1caddfba1fa43d095df4e5cb209d94842c6f
roger <roger.jaggi@blastbeatsyndicate.com>
2015-04-18 13:44:55 +0200

    initial code import

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # commandQueue
 Command Queue for synchronising commands and their responses. It supports also async messages.
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by roger on 4/18/15.
+ */
+
+var commandQueue = require('lib/commandQueue'),
+    bluegigaCommand = require('lib/bluegigaCommand'),
+    careU1AtCommand = require('lib/careU1AtCommand');
+
+exports = {
+    commandQueue: commandQueue.commandQueue,
+    processReturnCodes: commandQueue.processReturnCodes,
+    abstractCommand: commandQueue.abstractCommand,
+    bluegigaCommand: bluegigaCommand,
+    careU1AtCommand: careU1AtCommand
+};
